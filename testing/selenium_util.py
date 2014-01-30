844e7d0fea380bcabf8a2fd760564f38b09e5f4b
Anthony Sottile <asottile@umich.edu>
2014-01-30 05:41:34 -0800

    Make the window maximize before testing.

--- testing/selenium_util.py
+++ testing/selenium_util.py
@@ -1,31 +1,35 @@
 import functools
 import time
 from selenium.webdriver.remote import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class SlowWebdriver(webdriver.WebDriver):
 
-	def execute(self, driver_command, params=None):
-		time.sleep(0.5)
-		return super(SlowWebdriver, self).execute(driver_command, params)
+    def execute(self, driver_command, params=None):
+        time.sleep(0.5)
+        return super(SlowWebdriver, self).execute(driver_command, params)
 
 def with_driver(*desired_capabilities):
 
-	def decorator(func):
+    def decorator(func):
 
-		@functools.wraps(func)
-		def wrapper(test_case):
-			for dc in desired_capabilities:
-				driver = SlowWebdriver(
-					desired_capabilities=dc
-				)
+        @functools.wraps(func)
+        def wrapper(test_case):
+            for dc in desired_capabilities:
+                driver = SlowWebdriver(
+                    desired_capabilities=dc
+                )
 
-				driver.implicitly_wait(10)
+                # Opera YUNO maximize
+                if dc is not DesiredCapabilities.OPERA:
+                    driver.maximize_window()
+                driver.implicitly_wait(10)
 
-				try:
-					func(test_case, driver)
-				finally:
-					driver.close()
+                try:
+                    func(test_case, driver)
+                finally:
+                    driver.close()
 
-		return wrapper
+        return wrapper
 
-	return decorator
+    return decorator
